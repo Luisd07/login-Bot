4cea03c44aa36bf3d5cac58e7f63ea735b3541fe
Luis Diaz <luimd0728@gmail.com>
2020-04-02 00:40:44 -0400

    changed some of the code and added comments

--- loginBot.py
+++ loginBot.py
@@ -1,18 +1,26 @@
+# Bot to login to website
+
 from selenium import webdriver
+import os
 
+# usename and password stored in enviornment variable
+usernameStr = os.environ.get('botUser')
+passwordStr = os.environ.get('botPass')
 
-usernameStr = "0400990"
-passwordStr = "luism123"
-
+# tells selenium we are using chrome
 browser = webdriver.Chrome()
 
+# the website we are trying to log into
 browser.get(("https://auth.dadeschools.net/_auth/dsLogon.aspx?ru=aHR0cDovL21kY3BzcG9ydGFsLmRhZGVzY2hvb2xzLm5ldC8="))
 
+# finding the login box on the website and entering our usename
 username = browser.find_element_by_id('txtUsername')
 username.send_keys(usernameStr)
 
+# finding the password box and entering in our pasword
 password = browser.find_element_by_id('txtPassword')
 password.send_keys(passwordStr)
 
+# clicks the sign in button.
 signInButton = browser.find_element_by_id('btnLogon')
 signInButton.click()
